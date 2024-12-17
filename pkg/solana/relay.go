9ee89773227d19bc4aa9ae3f4e2593e856ac8f2d
Farber98 <juan.farber@smartcontract.com>
2024-12-17 17:54:07 -0300

    address feedback

--- pkg/solana/relay.go
+++ pkg/solana/relay.go
@@ -30,9 +30,8 @@ type TxManager interface {
 	// Important Notes:
 	// - The tx must contain at least one account key. The first account will be used to sign the tx (fee payer's public key).
 	// - txCfgs can be used to set custom tx configurations.
-	// - If an txID is provided, it will be used to identify the tx. Otherwise, a random UUID will be generated.
-	// - The caller needs to set the tx.Message.RecentBlockhash and provide the corresponding lastValidBlockHeight.
-	//   These values are obtained from the GetLatestBlockhash RPC call.
+	// - If a txID is provided, it will be used to identify the tx. Otherwise, a random UUID will be generated.
+	// - The caller needs to set the tx.Message.RecentBlockhash and provide the corresponding lastValidBlockHeight. These values are obtained from the GetLatestBlockhash RPC call.
 	Enqueue(ctx context.Context, accountID string, tx *solana.Transaction, txID *string, lastValidBlockHeight uint64, txCfgs ...txm.SetTxConfig) error
 }
 
